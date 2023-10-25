2c4eb38d59e62c29904bd6f9fa8996b72692ce86
Ilham_15 <116185926+Ilhambs14@users.noreply.github.com>
2023-10-25 11:48:11 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>gg</title>
-    </head>
-    <body>
-        hello world
-    </body>
+<?php
+// Create database connection using config file
+include_once("config.php");
+ 
+// Fetch all users data from database
+$result ="SELECT menu. *, penjual.nama_penjual FROM menu join penjual on menu.id_penjual = penjual.id_p ";
+$data = mysqli_query($mysqli, $result);
+
+?>
+ 
+<html>
+<head>    
+    <title>Menu Kantin</title>
+</head>
+ 
+<body>
+<a href="add.php">Tambah Pesanan</a><br/><br/>
+ 
+    <table width='80%' border=1>
+ 
+    <tr>
+     <th>Jenis</th> <th>Harga</th> <th>Nama</th> <th>Penjual</th><th>Update</th>
+    </tr>
+    <?php  
+    while($user_data = mysqli_fetch_array($data)) {         
+        echo "<tr>";
+        echo "<td>".$user_data['jenis']."</td>";
+        echo "<td>".$user_data['harga']."</td>";
+        echo "<td>".$user_data['nama']."</td>";   
+        echo "<td>".$user_data['nama_penjual']."</td>";   
+        echo "<td><a href='edit.php?id=$user_data[id]'>Edit</a> | <a href='delete.php?id=$user_data[id]'>Delete</a></td></tr>";        
+    }
+    ?>
+    </table>
+</body>
 </html>
